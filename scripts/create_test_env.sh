35a38af83bdf3a3857b2a3d8fdbe2a8044177250
Andreas Stuerz <andreas.stuerz@markt.de>
2020-04-02 15:10:43 +0200

    Prepare Module for Travis ci

---
+++ scripts/create_test_env.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+pdk bundle exec rake 'litmus:provision_list[default]' &&
+pdk bundle exec rake litmus:install_agent &&
+pdk bundle exec rake litmus:install_module
---
+++ spec/spec_helper_acceptance_local.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require 'puppet_litmus'
+require 'singleton'
+require 'tempfile'
+
+class LitmusHelper
+  include Singleton
+  include PuppetLitmus
+end
+
+RSpec.configure do |c|
+  c.before :suite do
+    LitmusHelper.instance.run_shell("puppet module install puppetlabs-apache")
+    rLitmusHelper.instance.run_shell("puppet module install puppetlabs-php")
+    #if os[:family] == 'debian' || os[:family] == 'ubuntu'
+    #  # needed for the puppet fact
+    #  apply_manifest("package { 'lsb-release': ensure => installed, }", expect_failures: false)
+    #end
+    ## needed for the grant tests, not installed on el7 docker images
+    #apply_manifest("package { 'which': ensure => installed, }", expect_failures: false)
+  end
+end
